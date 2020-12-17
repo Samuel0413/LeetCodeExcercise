bf3d070d959a91c85099286833236a05f81f63b2
Samuel0413 <samuel.0413kb@gmail.com>
2020-12-17 15:18:44 -0500

    c++ version update on 0005

---
+++ 0005_Longest_Palindromic/0005_Longest_Palindromic.cc
@@ -0,0 +1,59 @@
+/*
+Given a string s, return the longest palindromic substring in s.
+*/
+
+class Solution {
+private:
+    int expandFromCenter(string s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+public:
+    string longestPalindrome(string s) {
+        if (s.length() < 1) {
+            return "";
+        }
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromCenter(s, i, i);
+            int len2 = expandFromCenter(s, i, i+1);
+            int len = max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        cout<< start << endl;
+        cout<< end << endl;
+        return s.substr(start, end-start+1);
+    }
+};
+
+/*
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+Example 3:
+
+Input: s = "a"
+Output: "a"
+Example 4:
+
+Input: s = "ac"
+Output: "a"
+
+Some testcases:
+"ccc"
+"cccc"
+*/
