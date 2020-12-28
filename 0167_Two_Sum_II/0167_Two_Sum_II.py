7a48c053249758de2db494fa518c7aaa7629dbaf
Samuel0413 <samuel.0413kb@gmail.com>
2020-12-28 10:30:23 -0500

    0167 updates

---
+++ 0167_Two_Sum_II/0167_Two_Sum_II.py
@@ -0,0 +1,59 @@
+"""
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+"""
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            left_num = numbers[i]
+            right_num = numbers[j]
+            mid = int((i + j) / 2)
+            if left_num+right_num == target:
+                return [i+1, j+1]
+            elif left_num+right_num > target:
+                j = mid if numbers[i]+numbers[mid] >= target else j-1
+            else:
+                i = mid if numbers[j]+numbers[mid] <= target else i + 1
+
+        """
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            left_num = numbers[i]
+            right_num = numbers[j]
+            if left_num+right_num == target:
+                return [i+1, j+1]
+            elif left_num+right_num > target:
+                j -= 1
+            else:
+                i += 1
+        """
+            
+"""
+Example 1:
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+
+Example 2:
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+
+Example 3:
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+
+Time: O(n)
+"""
