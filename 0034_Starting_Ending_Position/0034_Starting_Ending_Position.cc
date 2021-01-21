5107d0a55c4afa49c6af79970d071f04c2de19a2
Samuel0413 <samuel.0413kb@gmail.com>
2021-01-20 22:29:39 -0500

    0034 updates

--- 0034_Starting_Ending_Position/0034_Starting_Ending_Position.cc
+++ 0034_Starting_Ending_Position/0034_Starting_Ending_Position.cc
@@ -1,5 +1,4 @@
-/*
- 
+/* 
 Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
 
 If target is not found in the array, return [-1, -1].
